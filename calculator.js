f9eb6249c0347a7ed9bc8a3590a3a02f85c12306
Carlos Eduardo <cadusikorski21@gmail.com>
2026-04-27 21:32:04 -0300

    at calculadora

--- calculator.js
+++ calculator.js
@@ -6,7 +6,88 @@
 // fluxo de excecução da calculadora
 // usuario digita um número => qual operação deseja fazer? => outro núnmero === resultado
 
-const prompt = require ('prompt-sync') ({});
+const prompt = require('prompt-sync')();
+
+console.log("**SEJA BEM VINDO A LIONS-CALCULATOR**")
+
+function input() {
+
+    const numero1 = Number(prompt('Digite o primeiro número: '))
+
+    const operacao = (prompt(`Agora, digite a operação("+", "-", "*", "/" e "%") R:`))
+
+    const numero2 = Number(prompt('Digite o segundo número: '))
+
+    return operacoes(operacao, numero1, numero2)
+
+}
+
+function operacoes(op, nm1, nm2) {
+
+    switch (op) {
+
+        case '+':
+            soma(nm1, nm2)
+            break;
+
+        case '-':
+            subtracao(nm1, nm2)
+            break;
+
+        case '*':
+            multiplicacao(nm1, nm2)
+            break;
+
+        case '/':
+            divisao(nm1, nm2)
+            break;
+
+        case '%':
+            porcentagem(nm1, nm2)
+            break;
+
+        case 'default': 
 
 
+    }
 
+    
+
+}
+
+function soma(id1, id2) {
+
+    let resultado = (id1 + id2)
+    return console.log(resultado.toFixed(2))
+
+}
+
+function subtracao(id1, id2) {
+
+    let resultado = (id1 - id2)
+    return resultado.toFixed(2)
+
+}
+
+function multiplicacao(id1, id2) {
+
+    let resultado = (id1 * id2)
+    return resultado.toFixed(2)
+
+}
+
+function divisao(id1, id2) {
+
+    let resultado = (id1 / id2)
+    return resultado.toFixed(2)
+
+}
+
+function porcentagem(id1, id2) {
+
+    let resultado = (id1) * (id2 / 100)
+    return resultado.toFixed(2)
+
+}
+
+input()
